f02844ad0049e798ee53db3cc1fa95d5fefdac93
Pritam Khergade <pritamkhergade@Pritams-MacBook-Air.local>
2024-07-29 10:07:03 +0530

    tf

---
+++ terraform/ec2/var.tf
@@ -0,0 +1,7 @@
+variable "ami" {}
+variable "instance_type" {}
+variable "subnet_id" {}
+variable "keyname" {}
+variable "name" {}
+variable "public_ip" {}
+variable "security_group" {}
---
+++ terraform/vpc/output.tf
@@ -0,0 +1,9 @@
+output "vpcid" {
+  value = aws_vpc.main.id
+}
+output "public_subnet_id" {
+  value = aws_subnet.public[0].id
+}
+output "private_subnet_id" {
+  value = aws_subnet.private[0].id
+}
