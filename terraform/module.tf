a39556bc108946854eb2c1bf987a26739cc22ee5
Pritam Khergade <pritamkhergade@Pritams-MacBook-Air.local>
2024-07-29 10:26:53 +0530

    eks

--- terraform/module.tf
+++ terraform/module.tf
@@ -30,3 +30,14 @@ module "sg" {
 
   }
 }
+
+module "eks" {
+  source        = "./eks"
+  cluster_name  = "ot-microservice-cluster"
+  subnet_ids    = module.vpc.private_subnet_id
+  instance_type = "t3.medium"
+  desired_size  = 1
+  max_size      = 5
+  min_size      = 1
+  node_group_subnet_id = module.vpc.private_subnet_id
+}
--- terraform/vpc/output.tf
+++ terraform/vpc/output.tf
@@ -5,5 +5,5 @@ output "public_subnet_id" {
   value = aws_subnet.public[0].id
 }
 output "private_subnet_id" {
-  value = aws_subnet.private[0].id
+  value = aws_subnet.private.id
 }
